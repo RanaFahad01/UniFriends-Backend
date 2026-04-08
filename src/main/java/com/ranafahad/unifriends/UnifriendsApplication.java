1f2a1826b923a1c8dd40dfadc6cf9727db48d5cc
Fahad_Ahmed <fahad@mailbox.unideb.hu>
2026-04-09 01:36:35 +0200

    initial commit - project generated through Spring Initializr

---
+++ src/main/java/com/ranafahad/unifriends/UnifriendsApplication.java
@@ -0,0 +1,13 @@
+package com.ranafahad.unifriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranafahad/unifriends/UnifriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranafahad.unifriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
